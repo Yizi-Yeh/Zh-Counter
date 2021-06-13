d854c35f94c7d0a71e8664c67a613dfac568e86f
Yizi-Yeh <g384357@gmail.com>
2021-06-13 13:17:12 +0800

    upadte : post form-data use qs

--- src/views/Counters/index.vue
+++ src/views/Counters/index.vue
@@ -5,27 +5,27 @@
             <el-row>
                 <el-col :xl="{span: 12, offset: 6}" :lg="{span: 12, offset: 6}" :md="{span: 16, offset: 4}" :sm="{span: 20, offset: 2}" :xs="24">
 
-            <el-form  :model="ruleForm"  status-icon :rules="rules" ref="ruleFormRef"
+            <el-form  :model="ruleForm"   :rules="rules" ref="ruleFormRef"
             @keyup.enter="submitForm" label-width="100px">
 
-                <el-form-item  label="名稱" prop="name">
-                  <el-input  placeholder="請輸入名稱" v-model="ruleForm.name"></el-input>
+                <el-form-item  label="名稱" prop="Name">
+                  <el-input  placeholder="請輸入名稱" v-model="ruleForm.Name"></el-input>
                  </el-form-item>
 
-                 <el-form-item label="描述" prop="description">
-                  <el-input  placeholder="請輸入描述"  v-model="ruleForm.description"></el-input>
+                 <el-form-item label="描述" prop="Description">
+                  <el-input  placeholder="請輸入描述"  v-model="ruleForm.Description"></el-input>
                  </el-form-item>
 
-            <el-form-item label="密碼" prop="password">
-                <el-input  placeholder="請輸入密碼" type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
+            <el-form-item label="密碼" prop="Password">
+                <el-input  placeholder="請輸入密碼" type="password" v-model="ruleForm.Password" autocomplete="off"></el-input>
             </el-form-item>
 
-            <el-form-item label="確認密碼" prop="confirmPassword">
-            <el-input  placeholder="請再次確認密碼" type="password" v-model="ruleForm.confirmPassword" autocomplete="off"></el-input>
+            <el-form-item label="確認密碼" prop="ConfirmPassword">
+            <el-input  placeholder="請再次確認密碼" type="password" v-model="ruleForm.ConfirmPassword" autocomplete="off"></el-input>
             </el-form-item>
 
-                 <el-form-item  label="限制人數" prop="limit">
-                  <el-input  placeholder="請輸入限制人數"  v-model.number="ruleForm.limit"></el-input>
+                 <el-form-item  label="限制人數" prop="Limit">
+                  <el-input  placeholder="請輸入限制人數"  v-model.number="ruleForm.Limit"></el-input>
                  </el-form-item>
 
                  <el-form-item>
@@ -44,16 +44,17 @@
 <script>
 import { ref, reactive, getCurrentInstance } from 'vue'
 import axios from 'axios'
+import qs from 'qs'
 export default ({
   setup () {
     const { ctx } = getCurrentInstance()
 
     const ruleForm = reactive({
-      name: '',
-      description: '',
-      password: '',
-      confirmPassword: '',
-      limit: ''
+      Name: '',
+      Description: '',
+      Password: '',
+      ConfirmPassword: '',
+      Limit: ''
     })
 
     const ruleFormRef = ref(null)
@@ -74,8 +75,8 @@ export default ({
       if (value === '') {
         callback(new Error('請輸入密碼'))
       } else {
-        if (ruleForm.password !== '') {
-          ctx.$refs.ruleForm.validateField('confirmPassword')
+        if (ruleForm.Password !== '') {
+          ctx.$refs.ruleForm.validateField('ConfirmPassword')
         }
         callback()
       }
@@ -84,7 +85,7 @@ export default ({
     const validatePass2 = (rule, value, callback) => {
       if (value === '') {
         callback(new Error('請再次輸入密碼'))
-      } else if (value !== ruleForm.password) {
+      } else if (value !== ruleForm.Password) {
         callback(new Error('兩次輸入密碼不一致!'))
       } else {
         callback()
@@ -92,16 +93,16 @@ export default ({
     }
 
     const rules = reactive({
-      password: [
+      Password: [
         { required: true, validator: validatePass, trigger: 'blur' }
       ],
-      confirmPassword: [
+      ConfirmPassword: [
         { required: true, validator: validatePass2, trigger: 'blur' }
       ],
-      name: [
+      Name: [
         { required: true, validator: checkName, trigger: 'blur' }
       ],
-      limit: [
+      Limit: [
         { required: true, validator: checkLimit, trigger: 'blur' }
       ]
     })
@@ -112,9 +113,11 @@ export default ({
           return false
         }
       })
-      axios.post('/api/Create', ruleForm)
+      axios.post('/api/Create/', qs.stringify(ruleForm), {
+        headers: { 'content-type': 'application/x-www-form-urlencoded' }
+      })
         .then(res => {
-          console.log(res.date)
+          console.log(res.data)
         })
         .catch(err => {
           console.error(err.data.error_msg)
