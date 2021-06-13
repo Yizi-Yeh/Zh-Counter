62b41625cdab005be5dc99a37fe75b911953eeb1
Yizi-Yeh <g384357@gmail.com>
2021-06-13 12:05:25 +0800

    update: form-validate

--- src/views/Counters/index.vue
+++ src/views/Counters/index.vue
@@ -5,30 +5,32 @@
             <el-row>
                 <el-col :xl="{span: 12, offset: 6}" :lg="{span: 12, offset: 6}" :md="{span: 16, offset: 4}" :sm="{span: 20, offset: 2}" :xs="24">
 
-            <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px">
+            <el-form  :model="ruleForm"  status-icon :rules="rules" ref="ruleFormRef"
+            @keyup.enter="submitForm" label-width="100px">
 
                 <el-form-item  label="名稱" prop="name">
-                  <el-input placeholder="請輸入名稱" v-model.trim="ruleForm.name"></el-input>
+                  <el-input  placeholder="請輸入名稱" v-model="ruleForm.name"></el-input>
                  </el-form-item>
 
                  <el-form-item label="描述" prop="description">
-                  <el-input placeholder="請輸入描述"  v-model.trim="ruleForm.description"></el-input>
+                  <el-input  placeholder="請輸入描述"  v-model="ruleForm.description"></el-input>
                  </el-form-item>
 
             <el-form-item label="密碼" prop="password">
-                <el-input placeholder="請輸入密碼" type="password" v-model.trim.trim="ruleForm.password" autocomplete="off"></el-input>
+                <el-input  placeholder="請輸入密碼" type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
             </el-form-item>
 
-            <el-form-item label="確認密碼" prop="checkPass">
-            <el-input placeholder="請再次確認密碼" type="password" v-model.trim="ruleForm.checkPass" autocomplete="off"></el-input>
+            <el-form-item label="確認密碼" prop="confirmPassword">
+            <el-input  placeholder="請再次確認密碼" type="password" v-model="ruleForm.confirmPassword" autocomplete="off"></el-input>
             </el-form-item>
 
                  <el-form-item  label="限制人數" prop="limit">
-                  <el-input placeholder="請輸入限制人數"  v-model.number="ruleForm.limit"></el-input>
+                  <el-input  placeholder="請輸入限制人數"  v-model.number="ruleForm.limit"></el-input>
                  </el-form-item>
 
                  <el-form-item>
-                 <el-button type="danger" @click="handRegisterFn">送出</el-button>
+                 <el-button type="info" @click="submitForm">送出</el-button>
+                 <el-button @click="resetForm('ruleFormRef')">重置</el-button>
                  </el-form-item>
 
                 </el-form>
@@ -40,20 +42,22 @@
 </template>
 
 <script>
-import { reactive } from 'vue'
+import { ref, reactive, getCurrentInstance } from 'vue'
 import axios from 'axios'
 export default ({
   setup () {
-    // post 表單
+    const { ctx } = getCurrentInstance()
+
     const ruleForm = reactive({
       name: '',
       description: '',
       password: '',
-      checkPass: '',
+      confirmPassword: '',
       limit: ''
     })
 
-    //
+    const ruleFormRef = ref(null)
+
     const checkName = (rule, value, callback) => {
       if (!value) {
         return callback(new Error('名稱為必填項目'))
@@ -61,7 +65,7 @@ export default ({
     }
 
     const checkLimit = (rule, value, callback) => {
-      if (value) {
+      if (!value) {
         return callback(new Error('限制人數為必填項目'))
       }
     }
@@ -71,7 +75,7 @@ export default ({
         callback(new Error('請輸入密碼'))
       } else {
         if (ruleForm.password !== '') {
-          this.$refs.register.validateField('checkPass')
+          ctx.$refs.ruleForm.validateField('confirmPassword')
         }
         callback()
       }
@@ -89,31 +93,40 @@ export default ({
 
     const rules = reactive({
       password: [
-        { validator: validatePass, trigger: 'blur' }
+        { required: true, validator: validatePass, trigger: 'blur' }
       ],
-      checkPass: [
-        { validator: validatePass2, trigger: 'blur' }
+      confirmPassword: [
+        { required: true, validator: validatePass2, trigger: 'blur' }
       ],
       name: [
-        { validator: checkName, trigger: 'blur' }
+        { required: true, validator: checkName, trigger: 'blur' }
       ],
       limit: [
-        { validator: checkLimit, trigger: 'blur' }
+        { required: true, validator: checkLimit, trigger: 'blur' }
       ]
     })
-    const handRegisterFn = () => {
-      axios
-        .post('/api/Create', ruleForm)
+
+    const submitForm = () => {
+      ctx.$refs.ruleFormRef.validate(valid => {
+        if (!valid) {
+          return false
+        }
+      })
+      axios.post('/api/Create', ruleForm)
         .then(res => {
-          console.log(res)
+          console.log(res.date)
         })
         .catch(err => {
-          console.log(err)
+          console.error(err.data.error_msg)
         })
     }
 
+    const resetForm = (ruleFormRef) => {
+      ctx.$refs.ruleFormRef.resetFields()
+    }
+
     return {
-      ruleForm, handRegisterFn, rules
+      ruleForm, submitForm, rules, resetForm, ruleFormRef
     }
   }
 })
